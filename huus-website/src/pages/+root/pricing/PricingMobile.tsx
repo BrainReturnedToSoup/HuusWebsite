1379b3d514f86574148ef17efeba5ad4dff188d0
nikkolas <minton.nikkolas@gmail.com>
2024-07-17 17:44:06 -0500

    tweaked the phone oriented pricing section to have more defined offer boxes

--- huus-website/src/pages/+root/pricing/PricingMobile.tsx
+++ huus-website/src/pages/+root/pricing/PricingMobile.tsx
@@ -1,7 +1,11 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../../../state/store";
 
-import { OFFER_LIST, SECTION_TITLE, SECTION_DESC } from "../../../enums/pricing";
+import {
+  OFFER_LIST,
+  SECTION_TITLE,
+  SECTION_DESC,
+} from "../../../enums/pricing";
 
 import "../../../App.css";
 
@@ -72,38 +76,40 @@ function OfferPhone({
   index,
 }: OfferMobileProps) {
   return (
-    <a
-      className="my-4 aspect-square w-full"
-      id={`pricing-mobile-container-${index}`}
-      href={redirect.route}
-    >
-      <div className="pricing-mobile-phone-container-grid grid h-full border-y-2 border-black">
-        <div className="flex items-center justify-start bg-black px-8 py-2">
-          <h2 className="lato-medium text-3xl text-white">{title}</h2>
+    <div className="w-full p-4">
+      <a
+        className="aspect-square w-full"
+        id={`pricing-mobile-container-${index}`}
+        href={redirect.route}
+      >
+        <div className="pricing-mobile-phone-container-grid grid h-full border-2 border-black">
+          <div className="flex items-center justify-start bg-black px-8 py-2">
+            <h2 className="lato-medium text-3xl text-white">{title}</h2>
+          </div>
+          <div className="px-8 py-4">
+            <p className="lato-medium">{desc}</p>
+          </div>
+          <div className="flex items-center justify-start px-8 py-2">
+            <h3 className="lato-bold text-6xl">${price}</h3>
+          </div>
+          <div className="pb-2 pl-12 pr-4 pt-4">
+            <ul className="lato-medium list-disc text-xl">
+              {bullets.map((bullet) => {
+                return <li className="m-2">{bullet}</li>;
+              })}
+            </ul>
+          </div>
+          <div className="flex items-center justify-end py-4 pr-6">
+            <label
+              className="lato-medium text-xl underline hover:cursor-pointer"
+              htmlFor={`pricing-mobile-container-${index}`}
+            >
+              Details
+            </label>
+          </div>
         </div>
-        <div className="px-8 py-4">
-          <p className="lato-medium">{desc}</p>
-        </div>
-        <div className="flex items-center justify-start px-8 py-2">
-          <h3 className="lato-bold text-6xl">${price}</h3>
-        </div>
-        <div className="pb-2 pl-12 pr-4 pt-4">
-          <ul className="lato-medium list-disc text-xl">
-            {bullets.map((bullet) => {
-              return <li className="m-2">{bullet}</li>;
-            })}
-          </ul>
-        </div>
-        <div className="flex items-center justify-end py-4 pr-6">
-          <label
-            className="lato-medium text-xl underline hover:cursor-pointer"
-            htmlFor={`pricing-mobile-container-${index}`}
-          >
-            Details
-          </label>
-        </div>
-      </div>
-    </a>
+      </a>
+    </div>
   );
 }
 
