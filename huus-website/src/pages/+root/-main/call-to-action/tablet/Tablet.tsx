4591af8fcde54267eb1842151de1035037db7ef3
nikkolas <minton.nikkolas@gmail.com>
2024-07-22 07:30:09 -0500

    added the 'call to action' section for tablets

--- huus-website/src/pages/+root/-main/call-to-action/tablet/Tablet.tsx
+++ huus-website/src/pages/+root/-main/call-to-action/tablet/Tablet.tsx
@@ -1,13 +1,30 @@
+import blackArrowRight from "../../../../../assets/arrow-right-black.svg";
+
+import "../../../../../App.css";
+
 export default function Tablet() {
   return (
     <div className="flex h-[500px] w-full flex-col items-center justify-center bg-black px-14 pb-16 xl:px-20">
-      <h2 className="mb-3 text-6xl text-white">Like what you see?</h2>
+      <h2 className="mb-6 text-center text-6xl text-white">
+        Like what you see?
+      </h2>
       <a
-        className={`mb-6 flex items-center justify-center py-1 pl-4 pr-2 text-2xl text-white underline hover:cursor-pointer`}
+        id="call-to-action-redirect"
+        className={`mb-6 flex items-center justify-center bg-white py-2 pl-4 pr-2 text-center hover:cursor-pointer`}
+        href="/contact"
       >
-        Schedule a consultation today!
+        <label
+          htmlFor="call-to-action-redirect"
+          className="mr-1 text-2xl hover:cursor-pointer"
+        >
+          Schedule a consultation today!
+        </label>
+        <img
+          src={blackArrowRight}
+          className="aspect-square h-[36px] hover:cursor-pointer"
+        ></img>
       </a>
-      <p className="w-[675px] text-center leading-7 text-white">
+      <p className="w-[675px] text-center text-xl leading-8 text-white">
         Random description here Random description here Random description here
         Random description here Random description here Random description here
         Random description here Random description here Random description here
