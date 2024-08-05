b66c2b1d4211892e4371bdf9f3cda23e0bf79965
nikkolas <minton.nikkolas@gmail.com>
2024-08-04 21:45:12 -0500

    fixed a bug with the desktop version of the feature list, so that each feature would extend to be the same height as the features list bar

--- huus-website/src/pages/+root/-main/features/Features.tsx
+++ huus-website/src/pages/+root/-main/features/Features.tsx
@@ -34,55 +34,57 @@ function Feature({ feature }: FeatureProps) {
   }
 
   return (
-    <a
-      href={feature.redirect.route}
-      className={`mx-1 my-1 flex flex-col justify-between p-4 transition-colors duration-300 ease-in-out md:my-3 lg:my-0 lg:h-full lg:max-w-[350px] ${sectionHovered && !redirectButtonHovered ? "bg-white" : ""}`}
-      onMouseEnter={handleSectionMouseEnter}
-      onMouseLeave={handleSectionMouseLeave}
-    >
-      <div className="flex flex-col sm:flex-row lg:block">
-        <div
-          className="mb-2 flex aspect-square min-w-[275px] flex-col bg-red-500 lg:min-w-fit"
-          style={{
-            backgroundImage: `url(${feature.backgroundImageProp})`,
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center",
-            backgroundSize: "cover",
-          }}
-        >
-          <h1 className="flex w-full items-center justify-center bg-black bg-opacity-75 p-2 text-3xl text-white sm:text-2xl">
-            {feature.title}
-          </h1>
-        </div>
-        <div className="flex flex-col justify-between">
-          <p
-            className={`mb-2 p-2 text-xl leading-relaxed transition-colors duration-300 ease-in-out sm:ml-4 md:p-4 lg:ml-0 lg:p-0 lg:text-base ${sectionHovered && !redirectButtonHovered ? "text-black" : "text-white"}`}
+    <div>
+      <a
+        href={feature.redirect.route}
+        onMouseEnter={handleSectionMouseEnter}
+        onMouseLeave={handleSectionMouseLeave}
+        className={`mx-1 my-1 flex flex-col items-center justify-between p-4 transition-colors duration-300 ease-in-out lg:my-0 lg:h-full lg:max-w-[350px] ${sectionHovered && !redirectButtonHovered ? "bg-white" : ""}`}
+      >
+        <div className="flex flex-col sm:flex-row lg:block">
+          <div
+            className="mb-2 flex aspect-square flex-col sm:min-w-[275px] lg:min-w-[330px]"
+            style={{
+              backgroundImage: `url(${feature.backgroundImageProp})`,
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+            }}
           >
-            {feature.desc}
-          </p>
-
-          <div className="hidden items-end justify-end p-2 sm:flex lg:hidden">
-            <button
-              onMouseEnter={handleRedirectButtonMouseEnter}
-              onMouseLeave={handleRedirectButtonMouseLeave}
-              className={`p-2 text-xl underline transition-colors duration-300 ease-in-out ${sectionHovered || redirectButtonHovered ? "bg-white text-black" : "text-white"}`}
+            <h1 className="flex w-full items-center justify-center bg-black bg-opacity-75 p-2 text-3xl text-white sm:text-2xl">
+              {feature.title}
+            </h1>
+          </div>
+          <div className="flex flex-col justify-between">
+            <p
+              className={`mb-2 p-2 text-xl leading-relaxed transition-colors duration-300 ease-in-out sm:ml-4 md:p-4 lg:ml-0 lg:p-0 lg:text-base ${sectionHovered && !redirectButtonHovered ? "text-black" : "text-white"}`}
             >
-              Learn more
-            </button>
+              {feature.desc}
+            </p>
+
+            <div className="hidden items-end justify-end p-2 sm:flex lg:hidden">
+              <button
+                onMouseEnter={handleRedirectButtonMouseEnter}
+                onMouseLeave={handleRedirectButtonMouseLeave}
+                className={`p-2 text-xl underline transition-colors duration-300 ease-in-out ${sectionHovered || redirectButtonHovered ? "bg-white text-black" : "text-white"}`}
+              >
+                Learn more
+              </button>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="flex grow items-end justify-end p-2 sm:hidden lg:flex">
-        <button
-          onMouseEnter={handleRedirectButtonMouseEnter}
-          onMouseLeave={handleRedirectButtonMouseLeave}
-          className={`px-2 py-1 text-xl underline transition-colors duration-300 ease-in-out lg:text-base ${sectionHovered || redirectButtonHovered ? "text-black" : "text-white"} ${redirectButtonHovered ? "bg-white" : ""}`}
-        >
-          Learn more
-        </button>
-      </div>
-    </a>
+        <div className="w-full items-end justify-end p-2 sm:hidden lg:flex">
+          <button
+            onMouseEnter={handleRedirectButtonMouseEnter}
+            onMouseLeave={handleRedirectButtonMouseLeave}
+            className={`px-2 py-1 text-xl underline transition-colors duration-300 ease-in-out lg:text-base ${sectionHovered || redirectButtonHovered ? "text-black" : "text-white"} ${redirectButtonHovered ? "bg-white" : ""}`}
+          >
+            Learn more
+          </button>
+        </div>
+      </a>
+    </div>
   );
 }
 
@@ -97,7 +99,7 @@ export default function Features() {
           {SECTION_DESC}
         </p>
       </div>
-      <div className="flex w-full flex-col items-center justify-center bg-black px-1 py-1 lg:flex-row lg:items-start lg:px-4 lg:py-2">
+      <div className="flex w-full flex-col items-center justify-center bg-black px-1 py-1 lg:flex-row lg:items-stretch lg:px-4 lg:py-2">
         {FEATURE_LIST.map((feature, index) => (
           <Feature feature={feature} key={index} />
         ))}
