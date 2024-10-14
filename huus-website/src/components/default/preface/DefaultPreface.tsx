9172d82b0065f64c6b7c307cac4ec534866f22cb
nikkolas <minton.nikkolas@gmail.com>
2024-10-13 19:21:49 -0500

    taking it a step further creating a 'DefaultPreface' component to use across certain pages

--- huus-website/src/components/default/preface/DefaultPreface.tsx
+++ huus-website/src/components/default/preface/DefaultPreface.tsx
@@ -1,3 +1,17 @@
-export default function DefaultPreface() {
+interface DefaultPrefaceProps {
+  title: string;
+  desc: string;
+}
 
-} 
+export default function DefaultPreface({ title, desc }: DefaultPrefaceProps) {
+  return (
+    <div className="flex max-w-[760px] flex-col items-center px-4 py-14 lg:py-20">
+      <h3 className="lato-medium mb-6 w-full text-center text-4xl lg:text-5xl">
+        {title}
+      </h3>
+      <p className="lato-medium w-full text-center text-xl leading-loose">
+        {desc}
+      </p>
+    </div>
+  );
+}
